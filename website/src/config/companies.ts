deafaf2ddd7bb283dddf195d09d5a92f86518012
Jared Michel <jared@mipke.com>
2025-07-28 22:51:30 -0500

    update metaplanet to use ragnar data

--- website/src/config/companies.ts
+++ website/src/config/companies.ts
@@ -182,6 +182,20 @@ export const companies: Company[] = [
         x: "DunderHodl",
       },
     ],
+    googleSheet: {
+      extractions: [
+        {
+          id: "ragnar",
+          title: "Ragnar Stats",
+          description:
+            "Data extracted from <a href='https://x.com/RoaringRagnar' target='_blank' rel='noopener noreferrer'>@RoaringRagnar</a>'s open-source <a href='https://docs.google.com/spreadsheets/d/1hzlHsDwhcwRr3cPrZZBlavMU3mFda1CX6gVHJvURhzE/edit?gid=963629592#gid=963629592' target='_blank' rel='noopener noreferrer'>Google Sheet</a>.",
+          spreadsheetId: "1hzlHsDwhcwRr3cPrZZBlavMU3mFda1CX6gVHJvURhzE",
+          ranges: ["'Ragnar Comparison'!A2:A70", "'Ragnar Comparison'!C2:C70"],
+          processor: ragnarProcessor,
+          renderLocation: "sidebar",
+        },
+      ],
+    },
   },
 ];
 
