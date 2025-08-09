ce29b0e898ec7c640a15d57429b406f6165391c8
Jared Michel <jared@mipke.com>
2025-08-09 15:48:44 -0500

    reduce mobile padding

--- website/src/components/GenericChart.tsx
+++ website/src/components/GenericChart.tsx
@@ -577,7 +577,7 @@ export const GenericChart: React.FC<GenericChartProps> = ({
       : null;
 
   return (
-    <div className="bg-gray-900/50 rounded-lg p-4 mb-6">
+    <div className="bg-gray-900/50 rounded-lg p-2 md:p-4 mb-6">
       {/* Inject responsive CSS */}
       {responsiveCSS && (
         <style dangerouslySetInnerHTML={{ __html: responsiveCSS }} />
